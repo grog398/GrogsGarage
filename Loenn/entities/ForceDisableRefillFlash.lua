825396955c66be4f068fca57c32e46ae305043a1
grog398 <codemagegames@gmail.com>
2026-01-16 21:17:35 -0500

    Force Disable Reflill Flash Controller

---
+++ Loenn/entities/ForceDisableRefillFlash.lua
@@ -0,0 +1,12 @@
+local controller = {}
+
+controller.name = "GrogsGarage/ForceDisableRefillFlash"
+controller.depth = 0
+controller.texture = "Loenn/GrogsGarage/ForceDisableRefillFlash"
+controller.placements = {
+    name = "ForceDisableRefillFlash",
+    data = {
+    }
+}
+
+return controller
